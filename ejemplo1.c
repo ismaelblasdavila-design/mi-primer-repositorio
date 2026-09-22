b8f0bef302327d64bdfb871cac5908c51e92f9ed
Ismael Blas Davila <ismaelblasdavila@gmail.com>
2026-09-22 17:33:28 -0300

    feat(): agregué la documentación

---
+++ ejemplo1.c
@@ -0,0 +1,10 @@
+/*Ejercicio 1. Escriba un programa que imprima en la terminal la cadena de texto “Hola mundo”. (utiliza un 
+editor de texto plano para escribir el código fuente.)*/
+
+#include <stdio.h>
+
+int main() {
+    printf("hello, Git!\n");
+
+    return 0;
+}
